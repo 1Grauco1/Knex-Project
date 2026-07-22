5e22d96994db964c906a3386eb21b74ea0e1391e
g_ant <g.ant.facul@gmail.com>
2026-07-22 13:48:45 -0300

    + changes

--- src/app.js
+++ src/app.js
@@ -28,5 +28,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`TechMart API running on http://localhost:${PORT}`);
+  console.log(`TechMart API running on http://localhost:${PORT}/docs`);
 });
